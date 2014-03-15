6443ca3d808e9f1038e2f2eb38539c7d29d4dc95
Dmitry Ivanov <id.ajantis@gmail.com>
2014-03-15 15:01:55 +0100

    client cluster bootstrapping. joining the server cluster on startup.

--- client/src/main/scala/com/amsscala/academy/client/ClientMain.scala
+++ client/src/main/scala/com/amsscala/academy/client/ClientMain.scala
@@ -1,5 +1,30 @@
-package com.amsscala.academy.client
+package com.amsscala.academy
+package client
+
+import common.GameServerSettings._
+
+import akka.actor.{Props, Address, ActorSystem}
+import com.typesafe.config.ConfigFactory
+import akka.cluster.Cluster
 
 object ClientMain extends App {
-  println("Hello client!")
+
+  val config = ConfigFactory.load().getConfig("client")
+  val system = ActorSystem(clusterName, config)
+
+  println("Client is started.")
+
+  val cluster = Cluster(system)
+  val serverAddress = Address("akka.tcp", clusterName, "127.0.0.1", 2552)
+
+  cluster.join(serverAddress)
+
+  cluster.registerOnMemberUp {
+    println()
+    println("Client joined the game server.")
+    println("Connecting to the server lobby.\n")
+
+    val clientMaster = system.actorOf(Props[ClientMaster], "clientmaster")
+    // clientMaster ! JoinServer
+  }
 }
