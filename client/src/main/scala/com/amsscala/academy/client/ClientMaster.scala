3faec2a999afe707d59b3e43a7c8844769885702
Dmitry Ivanov <id.ajantis@gmail.com>
2014-03-15 15:00:00 +0100

    two empty root actors for client and server

---
+++ client/src/main/scala/com/amsscala/academy/client/ClientMaster.scala
@@ -0,0 +1,11 @@
+package com.amsscala.academy
+package client
+
+import akka.actor.{Actor, ActorLogging}
+
+class ClientMaster extends Actor with ActorLogging {
+
+  def receive = {
+    case _ =>
+  }
+}
---
+++ server/src/main/scala/com/amsscala/academy/server/ServerMaster.scala
@@ -0,0 +1,11 @@
+package com.amsscala.academy
+package server
+
+import akka.actor.{Actor, ActorLogging}
+
+class ServerMaster extends Actor with ActorLogging {
+
+  def receive = {
+    case _ =>
+  }
+}
