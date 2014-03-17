baebd1a7de3dee00e988e43a511f898272d0f95b
Dmitry Ivanov <id.ajantis@gmail.com>
2014-03-17 02:56:44 +0100

    Registering of a client on the server. Server keeps track of all clients in a set. When client is registered, server tries to find a match for it immediately amongst other client with "Waiting" status.

--- server/src/main/scala/com/amsscala/server/ServerMaster.scala
+++ server/src/main/scala/com/amsscala/server/ServerMaster.scala
@@ -1,11 +1,42 @@
-package com.amsscala.server
+package com.amsscala
+package server
 
-import akka.actor.{ Actor, ActorLogging }
+import common.LobbyProtocol.Register
+import common.{ Waiting, ClientState }
+
+import scala.collection.mutable.Set
+import scala.util.Random
+
+import akka.actor.{ActorRef, Actor, ActorLogging}
+
 
 class ServerMaster extends Actor with ActorLogging {
 
+  private[this] val clients: Set[Client] = Set()
+
   def receive = {
-    case _ ⇒
+    case Register ⇒
+      val newClient = Client(sender, Waiting)
+      clients += newClient
+
+      log.info("Registered a new client {}", newClient)
+      log.info("Finding a match for {}",     newClient)
+
+      findMatch(newClient).foreach { matched =>
+        newClient.ref ! "You got a match :)"
+        matched.ref   ! "You got a match :)"
+
+        // TODO setup a new game here =)
+      }
+  }
+
+  private[this] def findMatch(client: Client): Option[Client] = {
+    val allMatches = clients.filter(c ⇒ c.state == Waiting && c.ref != client.ref).toList
+
+    if (allMatches.size > 0)
+      Some(allMatches(Random.nextInt(allMatches.size)))
+    else
+      None
   }
 }
 
