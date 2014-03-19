36598773d35ffbe72bf8886036f7f9129f664af8
Elmar Weber <elmar.weber@cupenya.com>
2014-03-19 22:41:26 +0100

    - added unit test for GameActor / fixed GameActor

--- server/src/main/scala/com/amsscala/server/GameActor.scala
+++ server/src/main/scala/com/amsscala/server/GameActor.scala
@@ -3,12 +3,15 @@ package com.amsscala.server
 import akka.actor._
 import scala.concurrent.duration._
 import com.amsscala.common._
+import com.amsscala.common.GameProtocol._
 
 class GameActor extends Actor with ActorLogging {
   import context._
 
   import GameActor._
-  import GameProtocol._
+
+  private var roundDelay: FiniteDuration = _
+  private var initNrOfRounds: Option[Int] = _
 
   private var gameId: String = _
   private var gameName: String = _
@@ -25,35 +28,46 @@ class GameActor extends Actor with ActorLogging {
   private var currentP1Answer: Option[RoundAnswer] = None
   private var currentP2Answer: Option[RoundAnswer] = None
 
+  private def gameLog(msg: String) = gameName + "/" + gameId + ": " + msg
+
   override def receive = {
-    case init: InitGame => {
+    case InitGameConfigurable(init, nrOfRounds, delay) => {
+      roundDelay = delay
+      initNrOfRounds = nrOfRounds
       gameId = init.id
       gameName = init.name
       p1 = init.p1
       p2 = init.p2
+      log.info(gameLog("Initialized game"))
       p1 ! StartGame(gameId, gameName)
       p2 ! StartGame(gameId, gameName)
+      log.info(gameLog("Started game"))
       become(prepare)
+    }
+    case init: InitGame => {
+      self ! InitGameConfigurable(init)
     }
   }
 
   private def prepare: Receive = {
     case PlayerReady => {
+      log.info(gameLog("Player " + sender + " send ready"))
       if (sender == p1) {
-        p1Ready == true
+        p1Ready = true
       } else if (sender == p2) {
-        p2Ready == true
+        p2Ready = true
       } else {
-        log.warning("Received ready message from player that is not in game: " + sender)
+        log.warning(gameLog("Received ready message from player that is not in game: " + sender))
       }
       if (p1Ready && p2Ready) {
         currentRound = 0
-        totalRounds = (Math.random() * (MAX_ROUNDS - MIN_ROUNDS)).toInt + MIN_ROUNDS
+        totalRounds = initNrOfRounds.getOrElse((Math.random() * (MAX_ROUNDS - MIN_ROUNDS)).toInt + MIN_ROUNDS)
         p1Score = 0
         p2Score = 0
 
+        log.info(gameLog("Players ready, starting game with " + totalRounds + " rounds"))
         become(game)
-        system.scheduler.scheduleOnce(1.second, self, RoundTrigger)
+        system.scheduler.scheduleOnce(roundDelay, self, RoundTrigger)
       }
     }
   }
@@ -64,10 +78,13 @@ class GameActor extends Actor with ActorLogging {
       currentP1Answer = None
       currentP2Answer = None
 
+      log.info(gameLog("Starting round " + currentRound))
+
       p1 ! StartRound(currentRound)
       p2 ! StartRound(currentRound)
     }
     case answer: RoundAnswer => {
+      log.debug(gameLog("Received answer from " + sender + ": " + answer))
       (sender, answer) match {
         case (p1, answer) if (currentP1Answer.isEmpty) =>
           currentP1Answer = Some(answer)
@@ -82,12 +99,13 @@ class GameActor extends Actor with ActorLogging {
         p1Score += p1RoundScore
         p2Score += p2RoundScore
 
-        p1 ! RoundResult(currentRound, currentP1Answer.get.answer, currentP2Answer.get.answer, p1Score, p2Score)
-        p2 ! RoundResult(currentRound, currentP2Answer.get.answer, currentP1Answer.get.answer, p2Score, p1Score)
+        p1 ! RoundResult(currentRound, currentP2Answer.get.answer, currentP1Answer.get.answer, p2Score, p1Score)
+        p2 ! RoundResult(currentRound, currentP1Answer.get.answer, currentP2Answer.get.answer, p1Score, p2Score)
 
         if (currentRound < totalRounds) {
-          system.scheduler.scheduleOnce(1.second, self, RoundTrigger)
+          system.scheduler.scheduleOnce(roundDelay, self, RoundTrigger)
         } else {
+          log.info(gameLog("Ended, final scores " + p1Score + " : " + p2Score))
           p1 ! EndOfGame(p2Score, p1Score)
           p2 ! EndOfGame(p1Score, p2Score)
           become(shutdown)
@@ -100,6 +118,7 @@ class GameActor extends Actor with ActorLogging {
   private def shutdown: Receive = {
     case ShutdownGame => {
       parent ! EndOfGame(p1Score, p2Score)
+      log.info(gameLog("Shutting down"))
       context stop self
     }
   }
@@ -109,7 +128,8 @@ object GameActor {
   val MAX_ROUNDS = 100
   val MIN_ROUNDS = 60
 
-  private sealed trait GameActorProtocol
-  private case object RoundTrigger extends GameActorProtocol
-  private case object ShutdownGame extends GameActorProtocol
+  private[server] sealed trait GameActorProtocol
+  private[GameActor] case object RoundTrigger extends GameActorProtocol
+  private[GameActor] case object ShutdownGame extends GameActorProtocol
+  private[server] case class InitGameConfigurable(init: InitGame, nrOfROunds: Option[Int] = None, roundDelay: FiniteDuration = 1.second) extends GameActorProtocol
 }
---
+++ server/src/test/scala/com/amsscala/server/GameActorTest.scala
@@ -0,0 +1,138 @@
+package com.amsscala.server
+
+import akka.actor._
+import akka.testkit._
+import org.specs2.mutable._
+import org.specs2.time.NoTimeConversions
+import com.amsscala.common._
+import com.amsscala.common.GameProtocol._
+import scala.concurrent.duration._
+import com.amsscala.server.GameActor._
+
+class GameActorTest extends Specification with NoTimeConversions {
+  //  private implicit val WAIT = 5.seconds
+  private val GAME_ID = randomId
+  private val GAME_NAME = "test-game"
+
+  "Game Actor" should {
+    "send start game messages to actors after init" in new DefaultContext {
+      gameActor ! InitGame(GAME_ID, GAME_NAME, p1Actor, p2Actor)
+      p1Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+      p2Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+    }
+
+    "not init twice" in new DefaultContext {
+      gameActor ! InitGame(GAME_ID, GAME_NAME, p1Actor, p2Actor)
+      p1Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+      p2Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+
+      gameActor ! InitGame(GAME_ID, GAME_NAME, p1Actor, p2Actor)
+      p1Probe.expectNoMsg
+      p2Probe.expectNoMsg
+    }
+
+    "init round when both players are ready" in new DefaultContext {
+      gameActor ! InitGame(GAME_ID, GAME_NAME, p1Actor, p2Actor)
+      p1Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+      p2Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+
+      p1Probe.send(gameActor, PlayerReady)
+      p2Probe.send(gameActor, PlayerReady)
+
+      p1Probe.expectMsg(StartRound(1))
+      p2Probe.expectMsg(StartRound(1))
+    }
+
+    "send round result after both players answer" in new DefaultContext {
+      doInit()
+
+      p1Probe.expectMsg(StartRound(1))
+      p2Probe.expectMsg(StartRound(1))
+
+      p1Probe.send(gameActor, RoundAnswer(1, Talk))
+      p2Probe.send(gameActor, RoundAnswer(1, Talk))
+
+      p1Probe.expectMsg(RoundResult(1, Talk, Talk, 2, 2))
+      p2Probe.expectMsg(RoundResult(1, Talk, Talk, 2, 2))
+    }
+
+    "send correct answer and score or other player" in new DefaultContext {
+      doInit()
+
+      p1Probe.expectMsg(StartRound(1))
+      p2Probe.expectMsg(StartRound(1))
+
+      p1Probe.send(gameActor, RoundAnswer(1, Talk))
+      p2Probe.send(gameActor, RoundAnswer(1, Silent))
+
+      p1Probe.expectMsg(RoundResult(1, Silent, Talk, 3, 0))
+      p2Probe.expectMsg(RoundResult(1, Talk, Silent, 0, 3))
+
+      p1Probe.expectMsg(StartRound(2))
+      p2Probe.expectMsg(StartRound(2))
+    }
+
+    "play consecutive rounds and calculate score" in new DefaultContext {
+      doInit()
+
+      p1Probe.expectMsg(StartRound(1))
+      p2Probe.expectMsg(StartRound(1))
+
+      p1Probe.send(gameActor, RoundAnswer(1, Talk))
+      p2Probe.send(gameActor, RoundAnswer(1, Talk))
+
+      p1Probe.expectMsg(RoundResult(1, Talk, Talk, 2, 2))
+      p2Probe.expectMsg(RoundResult(1, Talk, Talk, 2, 2))
+
+      p1Probe.expectMsg(StartRound(2))
+      p2Probe.expectMsg(StartRound(2))
+
+      p1Probe.send(gameActor, RoundAnswer(2, Talk))
+      p2Probe.send(gameActor, RoundAnswer(2, Talk))
+
+      p1Probe.expectMsg(RoundResult(2, Talk, Talk, 4, 4))
+      p2Probe.expectMsg(RoundResult(2, Talk, Talk, 4, 4))
+    }
+
+    "play full game" in new DefaultContext {
+      doInit(Some(50), 1.millisecond)
+
+      def doRound(roundNr: Int) {
+        p1Probe.expectMsg(StartRound(roundNr))
+        p2Probe.expectMsg(StartRound(roundNr))
+
+        p1Probe.send(gameActor, RoundAnswer(roundNr, Talk))
+        p2Probe.send(gameActor, RoundAnswer(roundNr, Talk))
+
+        p1Probe.expectMsg(RoundResult(roundNr, Talk, Talk, roundNr * 2, roundNr * 2))
+        p2Probe.expectMsg(RoundResult(roundNr, Talk, Talk, roundNr * 2, roundNr * 2))
+      }
+
+      (1 to 50).foreach(i => doRound(i))
+
+      p1Probe.expectMsg(EndOfGame(100, 100))
+      p2Probe.expectMsg(EndOfGame(100, 100))
+    }
+  }
+
+  abstract class DefaultContext
+    extends TestKit(ActorSystem("test-system"))
+    with ImplicitSender with After {
+
+    lazy val gameActor = system.actorOf(Props[GameActor])
+    lazy val p1Probe = TestProbe()
+    lazy val p1Actor = p1Probe.ref
+    lazy val p2Probe = TestProbe()
+    lazy val p2Actor = p2Probe.ref
+
+    def after = system.shutdown()
+
+    def doInit(nrOfRounds: Option[Int] = None, roundDelay: FiniteDuration = 1.second) = {
+      gameActor ! InitGameConfigurable(InitGame(GAME_ID, GAME_NAME, p1Actor, p2Actor), nrOfRounds, roundDelay)
+      p1Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+      p2Probe.expectMsg(StartGame(GAME_ID, GAME_NAME))
+      p1Probe.send(gameActor, PlayerReady)
+      p2Probe.send(gameActor, PlayerReady)
+    }
+  }
+}
