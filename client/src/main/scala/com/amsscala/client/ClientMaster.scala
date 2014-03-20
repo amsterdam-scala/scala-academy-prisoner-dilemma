5c7deba66c666afa3cb4e355f057a50c71e31cc6
Dmitry Ivanov <id.ajantis@gmail.com>
2014-03-20 20:42:44 +0100

    client actor schedules a message to start a new game after the previous one is ended

--- client/src/main/scala/com/amsscala/client/ClientMaster.scala
+++ client/src/main/scala/com/amsscala/client/ClientMaster.scala
@@ -6,6 +6,10 @@ import common.LobbyProtocol.Register
 import common.GameProtocol._
 
 class ClientMaster(server: ActorSelection) extends Actor with ActorLogging with Stash {
+  import scala.concurrent.duration._
+  import context.dispatcher
+
+  val delayBetweenGames: FiniteDuration = 5.seconds
 
   override def preStart(): Unit = {
     server ! Register
@@ -15,10 +19,13 @@ class ClientMaster(server: ActorSelection) extends Actor with ActorLogging with 
     case msg: InGameMsg           => player.forward(msg)
 
     case cmd @ EndOfGame(_, _, _) =>
-      log.debug("Game over. Waiting for another game...")
+      log.info("Game over. Waiting for another game...")
+      scheduleAskForGame()
       context.become(waiting)
       unstashAll()
       player ! PoisonPill
+
+    case AskForGame               => // Already playing. Do nothing.
 
     case _                        => stash()
   }
@@ -28,6 +35,14 @@ class ClientMaster(server: ActorSelection) extends Actor with ActorLogging with 
       val player = context.actorOf(Props(new PlayerActor(id, name, self)))
       context.become(playing(player))
       player.forward(cmd)
+
+    case cmd @ AskForGame          =>
+      log.info("Let's start another game...")
+      server ! cmd
+  }
+
+  private[this] def scheduleAskForGame(): Unit = {
+    context.system.scheduler.scheduleOnce(delayBetweenGames, self, AskForGame)
   }
 
   def receive = waiting
